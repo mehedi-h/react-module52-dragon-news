f860ad13ddb8478f1e07fcff3ea6047d2b772a74
mehedi-h <mehedihs2015@gmail.com>
2023-10-03 12:33:08 +0600

    create and install associate components

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,21 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBfF0_cxCxwkJMGs7B-glxqb0aM_dAzgAk",
+    authDomain: "react-module52-dragon-ne-21dec.firebaseapp.com",
+    projectId: "react-module52-dragon-ne-21dec",
+    storageBucket: "react-module52-dragon-ne-21dec.appspot.com",
+    messagingSenderId: "567408974660",
+    appId: "1:567408974660:web:a7b4dc70a72b09a2cad511"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
